897ae897506810db4449a78de60ced0921caef4b
Sean Andersen <smandersen13@gmail.com>
2018-03-04 15:35:25 -0500

    initial

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+var mySQL = require("mysql");
+var dotEnv = require("dotenv").config();
+
+var connection = mySQL.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PW,
+  database: "burgers_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
+module.exports = dotEnv;
